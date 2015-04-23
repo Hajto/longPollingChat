28aa682e01471d65d46071c228d09854330e86ae
Hajtosek <hajtosek@gmail.com>
2015-04-23 21:31:21 +0200

    Waiting partially working

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,7 +9,7 @@ import play.api.libs.json.{Json, JsError}
 import play.api.mvc._
 import model.JSONFormats.paperClipFormat
 import play.libs.Akka
-import scala.concurrent.{Future, Await, Promise}
+import scala.concurrent.{TimeoutException, Future, Await, Promise}
 import scala.concurrent.duration._
 
 object Application extends Controller {
@@ -31,18 +31,20 @@ object Application extends Controller {
     }
   }
 
+  implicit val timeout = Timeout(30 second)
+
   def poll(nick:String, timestamp: Int) = Action{
+    val promiseOfResult = waitForList(nick, timestamp)
+    try {
+      val outCome = Await.result(promiseOfResult.future, 30 second).asInstanceOf[List[ChatMessage]]
+      Ok(Json.toJson(outCome))
+    } catch {
+      case e: TimeoutException => Ok("Pupa, nie masz wiadomości")
+    }
 
-    val messagePromise = waitForList(nick, timestamp)
-
-
-
-    Ok("")
   }
 
   def waitForList(nick: String, timestamp: Int) : Promise[List[ChatMessage]]  = {
-    implicit val timeout = Timeout(30 second)
-
     Await.result(messagingActor.ask(Subscribe(nick, timestamp)),30 second).asInstanceOf[Promise[List[ChatMessage]]]
   }
 
